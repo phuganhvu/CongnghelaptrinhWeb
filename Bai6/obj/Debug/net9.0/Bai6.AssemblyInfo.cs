8665ff2d2a3d86a8cb12481c42abe1aafa8aa823
Phuganhvu <vuphuonganh749@gmail.com>
2026-05-30 14:40:51 +0700

    Bai tap

--- Bai6/obj/Debug/net9.0/Bai6.AssemblyInfo.cs
+++ Bai6/obj/Debug/net9.0/Bai6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d08ab5086ef2595b5973077065bdf6a4b8e9e1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
